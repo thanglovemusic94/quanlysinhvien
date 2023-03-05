85e5af72146481ec1013b7b7853a6d150bbf0cb9
thanglovemusic94 <thanglovemusic94@gmail.com>
2023-03-05 22:10:20 +0700

    first commit

---
+++ src/main/java/thangtony/quanlysinhvien/QuanlysinhvienApplication.java
@@ -0,0 +1,13 @@
+package thangtony.quanlysinhvien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlysinhvienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanlysinhvienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thangtony/quanlysinhvien/QuanlysinhvienApplicationTests.java
@@ -0,0 +1,13 @@
+package thangtony.quanlysinhvien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlysinhvienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
